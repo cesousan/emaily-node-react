0d3b5605c10a3a8f82485a003418b1891f341507
cesousan <cesar.sabater@tiny-atom.com>
2018-04-28 16:53:17 +0200

    oauth flow complete and production settings ok

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 // nothing to return from passport file so we
 // just require it without assigning to a const
@@ -12,6 +14,14 @@ mongoose.connect(keys.mongoURI);
 
 // constructs the server
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 // imports the exported function and pass
 // it the app as an argument
 require('./routes/auth-routes')(app);
--- services/passport.js
+++ services/passport.js
@@ -5,15 +5,36 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user =>
+    user
+    ? done(null, user)
+    : done('no user with this id.', null)
+  );
+});
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId : profile.id }).save();
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if(existingUser) {
+            done(null, existingUser);
+          } else {
+            new User({ googleId : profile.id }).save()
+              .then(newUser => done(null, newUser));
+          }
+        })
     }
   )
 );

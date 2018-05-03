0d1829b80faef6091593635dd6badbdefe9e04da
csabater <csabater@hitpart.fr>
2018-05-03 10:25:07 +0200

    creation of header component and authReducer

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a class="brand-logo center">Header</a>
+        </div>
+      </nav>
+    );
+  };
+}
+
+export default Header;

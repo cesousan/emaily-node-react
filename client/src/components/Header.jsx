c487381dab92d53d673dc4ab69d14cae486f99ac
Unknown <cesar.sabater@tiny-atom.com>
2018-05-19 20:21:27 +0200

    handling authentication from client side of the app.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null :
+        return;
+      case false :
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default :
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    console.log(this.props);
     return (
       <nav>
-        <div class="nav-wrapper">
-          <a class="brand-logo center">Header</a>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            {this.renderContent()}
+
+          </ul>
         </div>
       </nav>
     );
   };
+
 }
 
-export default Header;
+// { auth } is ES6 destructuring for state.auth
+const mapStateToProps = ({ auth }) => { return { auth } };
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,16 @@
-export default function (state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+// we use a default state of null instead of {} so
+// while waiting for the ajx response, the state is
+// null and we don't display a specific state in components.
+export default function (state = null, action) {
+
   switch (action.type) {
+    // '||' syntax uses the fact that in js,
+    // an empty string (the return value when user
+    // is !authenticated) returns false.
+    case FETCH_USER :
+      return action.payload || false;
     default:
       return state;
   }
